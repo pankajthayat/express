7aabd8a8deeb2e5ff93bea4d29f04ccd00ede967
pankaj <pankajthayat@gmail.com>
2019-02-06 08:18:59 -0800

    setup change

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -2,12 +2,17 @@
 
 const MongoClient=require("mongodb").MongoClient;// pull off mongo client from mongodb lib
 //  no need to create before using it like others..just give name u want.. mongo will not create db until we start using
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+const client=new MongoClient('mongodb://localhost:27017');
+client.connect((err)=>{
     if(err){
         return console.log("unable to connect to mongodb server");
     }
     console.log("connected to mongodb server");
 
+    const db=client.db();
+    db.collection('Todos').insertOne({
+        text:"something"
+    })
 
-    db.close();
+    client.close();
 });// takes 2 string.. url where db lives.. in prod aws,heroku...here its local, 2nd arg is callback fn
